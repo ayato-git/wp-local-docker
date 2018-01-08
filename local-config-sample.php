89de17281398f03d2a15e930e2877649b1bf5f15
ayato-git <azazeru+github@gmail.com>
2018-01-09 02:55:57 +0900

    [delete] combine local-config.php into wp-config.php

--- local-config-sample.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_NAME',           getenv('MYSQL_DATABASE') );
-define('DB_USER',           getenv('MYSQL_USER') );
-define('DB_PASSWORD',       getenv('MYSQL_PASSWORD') );
-define('DB_HOST',           getenv('MYSQL_HOST') );
-define('WP_POST_REVISIONS', 3);
-define('WP_DEBUG',          (bool) getenv('WP_DEBUG') );
